b3fecdbdd6b0c4ea8b026ce4900a8e1115930729
Yongmin Park <75519420+oio337a@users.noreply.github.com>
2023-11-01 23:29:47 +0900

    [Gold II] Title: 후위 표기식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+trans_s = list(input())
+stack=[]
+res=''
+for s in trans_s:
+    if s.isalpha():
+        res+=s
+    else:
+        if s == '(':
+            stack.append(s)
+        elif s == '*' or s == '/':
+            while stack and (stack[-1] == '*' or stack[-1] =='/'):
+                res += stack.pop()
+            stack.append(s)
+        elif s == '+' or s == '-':
+            while stack and stack[-1] != '(':
+                res+= stack.pop()
+            stack.append(s)
+        elif s == ')':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.pop()
+while stack :
+    res+=stack.pop()
+print(res)
