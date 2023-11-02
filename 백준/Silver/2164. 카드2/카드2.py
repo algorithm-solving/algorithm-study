f9295188a2eb42c062d51f7583a1934de9452555
Yongmin Park <75519420+oio337a@users.noreply.github.com>
2023-11-02 15:29:47 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 50924 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -4,11 +4,29 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+# n = int(input())
+# arr = deque(i + 1 for i in range(n))
+
+# while len(arr) != 1:
+#   arr.popleft()
+#   arr.append(arr.popleft())
+
+# print(arr[0])
+
+
+''' 버전 2 '''
+
 n = int(input())
 arr = deque(i + 1 for i in range(n))
+result = 0
 
-while len(arr) != 1:
-  arr.popleft()
-  arr.append(arr.popleft())
+while True:
+  result = arr.popleft()
+  try:
+    arr.append(arr.popleft())
+  except:
+    break
+print(result)
 
-print(arr[0])
+
+
