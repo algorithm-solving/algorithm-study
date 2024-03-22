6623ba8e033dd303fbfa59cde4fd030c8f7e896d
oio337a <oio337a@gmail.com>
2024-03-22 17:49:55 +0900

    Delete mis-created directory

--- 프로그래머스/lv1/59415. 최댓값 구하기/최댓값 구하기.sql
+++
@@ -1,2 +0,0 @@
--- 코드를 입력하세요
-SELECT datetime from animal_ins order by datetime desc limit 1
--- 프로그래머스/lv3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
+++
@@ -1,9 +0,0 @@
--- 코드를 입력하세요
-SELECT ORDER_ID,PRODUCT_ID, DATE_FORMAT(OUT_DATE, '%Y-%m-%d') OUT_DATE,
-CASE
-    WHEN DATE_FORMAT(OUT_DATE, '%Y-%m-%d') <= '2022-05-01' THEN '출고완료'
-    WHEN OUT_DATE IS NULL THEN '출고미정'
-    ELSE '출고대기'
-END 출고여부
-FROM FOOD_ORDER
-ORDER BY 1
